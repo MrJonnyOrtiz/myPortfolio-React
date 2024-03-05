4ebedbde41d36949755aad4645e8a937c94cf995
Jonny Ortiz <j0nny.0rt1z@yahoo.com>
2024-03-05 15:49:22 -0500

    added fullstack image

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,10 +12,14 @@ function Home() {
   return (
     <div className="grid gap-8 text-center">
       <h1 className="text-3xl md:text-5xl lg:text-7xl">
-        Welcome to my online portfolio!
+        Welcome to my portfolio!
       </h1>
       <Bio />
       {/* <Carousel slides={projects} /> */}
+      <img
+        src="/fullStackVisual.png"
+        className="mx-auto hidden h-auto w-full rounded-xl sm:block sm:w-full md:w-3/4 lg:w-1/2"
+      />
     </div>
   );
 }
