d482a7713c54a5feb654d68c9c60bd9a49749093
Jonny Ortiz <j0nny.0rt1z@yahoo.com>
2024-09-27 18:13:51 -0400

    improvement: ui/ux navbar

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -4,12 +4,12 @@ import PropTypes from 'prop-types';
 
 const DropdownMenu = ({ items, handleDropdownVisible }) => {
   return (
-    <div className="absolute -left-8 right-6 top-12 z-10">
+    <div className="absolute -left-8 right-6 top-12 z-50">
       <ul className="grid w-[240%] gap-3 rounded-lg bg-white/90 p-3 shadow-lg">
         {items.map((item) => (
           <li
             key={item}
-            className="mx-auto w-full rounded-full bg-green-300 px-4 py-2 font-semibold text-gray-800 transition duration-300 hover:bg-green-400"
+            className="mx-auto w-full rounded-full bg-green-300 px-4 py-2 font-semibold text-gray-800 shadow-sm transition duration-300 hover:bg-green-400 hover:shadow-xl"
             onClick={() => handleDropdownVisible((visible) => !visible)}
           >
             <Link to={`/${item.toLowerCase()}`}>{item}</Link>
